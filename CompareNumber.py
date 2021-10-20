c19a277f174235b2863e98ec0842c14755b8a4df
sudhira-se <sudhi.maddula@gmail.com>
2021-10-20 12:28:25 -0500

    My First Python files to GIT

---
+++ CompareNumber.py
@@ -0,0 +1,7 @@
+x=100
+y=500
+
+if x > y:
+    print('x is greater than y', (x))
+else:
+    print('y is greater than x', (y))
---
+++ MaxNumberFunction.py
@@ -0,0 +1,9 @@
+def maxFunction(firstNumber, secondNumber):
+    if firstNumber > secondNumber:
+        print("firstNumber is greater than secondNumber", (firstNumber))
+        print((firstNumber) , "is greater than ", (secondNumber))
+    else: 
+        print("secondNumber is greater than firstNumber", (secondNumber))
+        print((secondNumber) , "is greater than ", (firstNumber))
+
+maxFunction(2000,2500)
