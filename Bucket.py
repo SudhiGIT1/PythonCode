e640d908f23a39b3ef42a9c788d9b6a00da570ea
SudhiGIT1 <DETraining9@gmail.com>
2021-10-25 11:18:06 -0500

    Adding movies.py

---
+++ Bucket.py
@@ -0,0 +1,24 @@
+# Accessing AWS Bucket from Python
+import boto3
+import pandas
+
+# get boto3 client
+client = boto3.client(
+    's3',
+    aws_access_key_id = 'AKIAX3PIOPZV2JFE76FI',
+    aws_secret_access_key = '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',
+    region_name = 'us-east-2'
+)
+
+# Fetch the list of existing buckets
+clientResponse = client.list_buckets()
+
+#print(clientResponse)
+
+#print('Printing bucket names...')
+for bucket in clientResponse['Buckets']:
+    print(f'Bucket Name: {bucket["Name"]}')
+
+#print object name from the bucket
+csv_obj = client.get_object(Bucket='test-10232021-first', Key='movies.csv')
+
